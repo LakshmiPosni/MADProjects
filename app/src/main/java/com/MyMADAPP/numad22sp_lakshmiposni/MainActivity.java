c0cf775067fc57348045c63aacbfe24362be95a4
LakshmiPosni <posni.l@northeastern.edu>
2022-02-06 18:28:48 -0800

    A3 App Updates

--- app/src/main/java/com/MyMADAPP/numad22sp_lakshmiposni/MainActivity.java
+++ app/src/main/java/com/MyMADAPP/numad22sp_lakshmiposni/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,17 +16,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button aboutMeBTN = findViewById(R.id.button);
+        Button clickyClicky = findViewById(R.id.button2);
 
         aboutMeBTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                aboutMe();
+                Toast.makeText(getApplicationContext(), "Lakshmi Posni (posni.l@northeastern.edu)", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        clickyClicky.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                clickyClicky();
             }
         });
     }
 
-    private void aboutMe(){
-        Intent intent = new Intent(this, AboutMe.class);
+//    private void aboutMe(){
+//        Intent intent = new Intent(this, AboutMe.class);
+//        startActivity(intent);
+//    }
+
+    private void clickyClicky(){
+        Intent intent = new Intent(this, ClickyClicky.class);
         startActivity(intent);
     }
+
 }
